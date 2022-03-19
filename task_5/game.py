9de31294f39178fd1f1b5b891f621ceee7de9526
Serhii Matsyshyn <serhii.matsyshyn@ucu.edu.ua>
2022-03-19 21:52:23 +0200

    task 5 - modified classes

--- task_5/game.py
+++ task_5/game.py
@@ -48,36 +48,42 @@ class Room:
         return self.linked_rooms.get(command, self)
 
 
-class Enemy:
-    """ Enemy class """
-    defeated = 0
+class Character:
+    """ Character class """
 
     def __init__(self, name, description=""):
         self.name = name
         self.description = description
         self.conversation = None
-        self.weakness = None
 
     def set_description(self, description):
-        """ Set enemy description """
+        """ Set character description """
         self.description = description
 
     def set_conversation(self, conversation):
-        """ Set enemy conversation """
+        """ Set character conversation """
         self.conversation = conversation
+
+    def describe(self):
+        """ Print character description """
+        print(f"{self.name} is here! - {self.description}")
+
+    def talk(self):
+        """ Character talks """
+        print(f"[{self.name} says]: {self.conversation}")
+
+
+class Enemy(Character):
+    """ Enemy class """
+    defeated = 0
+
+    def __init__(self, name, description=""):
+        super().__init__(name, description)
+        self.weakness = None
 
     def set_weakness(self, weakness):
         """ Set enemy weakness """
         self.weakness = weakness
-
-    def describe(self):
-        """ Print enemy description """
-        print(f"{self.name} is here!")
-        print(self.description)
-
-    def talk(self):
-        """ Enemy talks """
-        print(f"[{self.name} says]: {self.conversation}")
 
     def fight(self, fight_with):
         """ Fight with enemy """
@@ -93,6 +99,25 @@ class Enemy:
     def get_defeated(cls):
         """ Get number of defeated enemies """
         return Enemy.defeated
+
+
+class Friend(Character):
+    """ Friend Character class """
+
+    def __init__(self, name, description="Your friend."):
+        super().__init__(name, description)
+
+        self.advice = None
+        self.conversation = "I can give you advice!"
+
+    def set_advice(self, advice):
+        """ Set friend advice """
+        self.advice = advice
+
+    def talk(self):
+        """ Character talks """
+        print(f"[{self.name} says]: {self.conversation}")
+        print(f"[{self.name} says]: {self.advice}")
 
 
 class Item:
